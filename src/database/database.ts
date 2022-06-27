5f2d30aff4fddbb4f846c6d49973db3ad3684d6c
Raghav Singh <pr.raghavsingh@gmail.com>
2022-06-27 20:43:07 +0530

    Database url changed

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,7 +1,7 @@
 import * as mongoose from "mongoose";
-const url = "mongodb://localhost:27017/Test";
+// const url = "mongodb://localhost:27017/Test";
 // For docker environment
-// const url = "mongodb+srv://test:test123@cluster0.z3hwh.mongodb.net/?retryWrites=true&w=majority"; 
+const url = "mongodb+srv://test:test123@cluster0.z3hwh.mongodb.net/?retryWrites=true&w=majority"; 
 
 export class MongoConnection {
     private db: mongoose.Mongoose;
